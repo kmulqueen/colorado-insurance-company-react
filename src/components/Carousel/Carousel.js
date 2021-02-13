f478ebf65a7dc59f2ee4bbe8d9e91657c4a6ae33
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-02-13 13:22:29 -0700

    Updated carousel alts.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -8,21 +8,33 @@ const ImageCarousel = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100" src={rockClimber} alt="First slide" />
+        <img
+          className="d-block w-100"
+          src={rockClimber}
+          alt="Silhouette of a person climbing a large rock while the sun is setting"
+        />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={hiker} alt="Second slide" />
+        <img
+          className="d-block w-100"
+          src={hiker}
+          alt="Person hiking to the peak of a mountain."
+        />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={paddleBoard} alt="Third slide" />
+        <img
+          className="d-block w-100"
+          src={paddleBoard}
+          alt="Two people on paddleboards in the middle of a lake surrounded by mountains."
+        />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
